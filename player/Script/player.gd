9e98b9191b79179f38a7a418c90d98b3f9febb60
Habimana38 <realteddy38@gmail.com>
2024-01-22 16:44:04 +0100

    Day 1/ player movement

---
+++ player/Script/player.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+var screen_size
+var input : Vector2;
+
+@export var maxSpeed = 300;
+@export var acceleration = 100;
+@export var friction = 50; 
+
+@export var move_up : String;
+@export var move_down : String;
+@export var move_left : String;
+@export var move_right : String;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	input = Vector2.ZERO  # Réinitialiser le vecteur d'entrée à chaque frame
+
+	if Input.is_action_pressed(move_right):
+		input.x += 1
+	if Input.is_action_pressed(move_left):
+		input.x -= 1
+	if Input.is_action_pressed(move_down):
+		input.y += 1
+	if Input.is_action_pressed(move_up):
+		input.y -= 1
+	
+
+func _physics_process(delta):
+	
+	if input.length_squared() == 0 :
+		apply_friction(friction * delta)
+	else :
+		apply_acceleration(delta * acceleration * input.normalized())
+		
+	move_and_slide()
+
+
+func apply_friction(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+		
+func apply_acceleration(accel):
+	print(accel);
+	velocity += accel;
+	velocity = velocity.limit_length(maxSpeed)
+
